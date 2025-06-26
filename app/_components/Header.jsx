45cfe1014aa59a19e39a9ff63320073ada581905
Pi-Pika <piash15-5606@diu.edu.bd>
2025-06-27 01:17:53 +0600

    initial commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+function Header() {
+  return (
+    <div className='p-5 flex justify-between items-center border shadow-md'>
+        <Image src={'./logo.svg'}
+        alt='logo'
+        width={160}
+        height={100}
+        />
+        <Button>Get Started</Button>
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
