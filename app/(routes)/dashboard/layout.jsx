c4f84b03068592487da09ceb756dde9982c7dda9
Pi-Pika <piash15-5606@diu.edu.bd>
2025-07-15 09:06:44 +0600

    default location set

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+"use client"
+import React, { use, useEffect } from 'react'
 import SideNav from './_components/SideNav'
 import DashboardHeader from './_components/DashboardHeader'
+import { db } from '@/utils/dbConfig'
+import { Budgets } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
+import { useRouter } from 'next/navigation'
+
 function DashboardLayout({children}) {
+  
+  const {user} = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    user&&checkUserBudgets();
+  }, [user]);
+
+  const checkUserBudgets=async () =>{
+    const result =await db.select()
+    .from(Budgets)
+    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
+    
+    console.log(result);
+
+    if (result?.length === 0) {
+      router.replace('/dashboard/budgets');
+    }
+  }
+
   return (
     <div>
       <div className='fixed md:wd-64 hidden md:block'>
--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,7 @@ export default {
   out: "./drizzle", 
   dialect: "postgresql", 
   dbCredentials: {
-    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL ,
+    // connectionString: process.env.NEXT_PUBLIC_DATABASE_URL ,
+    url: 'postgresql://neondb_owner:npg_IOjDJZcK81dB@ep-hidden-math-adjl8wyj-pooler.c-2.us-east-1.aws.neon.tech/Expense%20Tracker?sslmode=require&channel_binding=require' ,
   },
 };
