235f23f373e47aa3e78a50aa314df78e38b9494f
faharia5891 <125296133+faharia5891@users.noreply.github.com>
2025-07-28 02:55:39 +0600

    Created Dashboard (Cards)

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { UserButton } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs';
+import CardInfo from './_components/Cardinfo'
+import { db } from '@/utils/dbConfig';
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
+import { Budgets, Expenses } from '@/utils/schema';
+
 
 function Dashboard() {
+  const {user}= useUser();
+
+  const [budgetList, setBudgetList] = useState([]);
+  
+    
+    useEffect(() => {
+      user&&getBudgetList();
+    }, [user])
+  
+    // Get budgetlist
+  
+    const getBudgetList=async()=> {
+      const result = await db.select({
+
+        ...getTableColumns(Budgets),
+        totalSpend: sql `sum(${Expenses.amount})`.mapWith(Number),
+        totalItem: sql `count(${Expenses.id})`.mapWith(Number)
+      }).from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id))
+      ;
+  
+      setBudgetList(result);
+    };
+
   return (
-    <div>
-      Dashboard
+    <div className= 'p-8'>
+      <h2 className='font-bold text-3xl'>Hi, {user?.fullName} 💙</h2>
+      <p className= 'text-gray-500'>Here's what happenning with your money, Let's manage your expenses.</p>
+
+      <CardInfo budgetList={budgetList} />
     </div>
+  
   )
 }
 
